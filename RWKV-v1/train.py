1fb321d407c792611bea33d63dd5070f83524d38
Cyan-离 <115965270+LemonFan-maker@users.noreply.github.com>
2022-12-20 12:35:43 +0800

    Update train.py

--- RWKV-v1/train.py
+++ RWKV-v1/train.py
@@ -22,7 +22,7 @@ model_type = 'RWKV'
 
 # datafile = u"V:\\NLP\\text8"
 # datafile = u"V:\\NLP\\enwik8"
-datafile = u"V:\\NLP\\simplebooks\\simplebooks-92-raw\\train.txt"
+datafile = u"../data.txt"
 datafile_encoding = 'utf-8'
 # datafile = u"D:\\NLP-Data\\ww100M.txt"
 # datafile = u"D:\\NLP-Data\\__2019.txt"
